086e82efbee1e5e7afa42fd7f2955007a7139623
Bruno Dimon <brunojdbsl@gmail.com>
2023-10-06 19:19:54 -0300

    Created the sales/purchase repositorys and fixed the both class to accept the @Entity

---
+++ MedStock - Backend/src/main/java/com/app/medStock/repository/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.app.medStock.repository;
+
+import com.app.medStock.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
